9db01ebc4eb8599d8d2094fe4cf8b39f36f30ae8
MartynasMaciulevicius <visiskaj.slaptaj@gmail.com>
2015-09-10 14:28:13 +0300

    PixelGenerator works as expected
    New expectations are satisfied

--- src/main/scala/mm/bitmap/gen/PixelGenerator.scala
+++ src/main/scala/mm/bitmap/gen/PixelGenerator.scala
@@ -1,11 +1,11 @@
 package mm.bitmap.gen
 
-import scala.collection.immutable.NumericRange
+import org.apache.commons.math3.complex.Complex
 
 /**
  * @author Martynas Maciulevičius.
  * @version 1.0 2015-09-05
  */
 trait PixelGenerator {
-  def generate(imageWidth: Int, imageHeight: Int, pxToCoord: NumericRange[Double]): Array[Int]
+  def generate(imageWidth: Int, imageHeight: Int, pxToCoord: Iterator[Complex]): Array[Int]
 }
--- src/main/scala/mm/bitmap/gen/RealPixelGenerator.scala
+++ src/main/scala/mm/bitmap/gen/RealPixelGenerator.scala
@@ -1,20 +1,23 @@
-//package mm.bitmap.gen
-//
-//import mm.bitmap.counters.Counter
-//
-//import scala.collection.immutable.NumericRange
-//
-///**
-// * sifjdfg
-// * @author Martynas Maciulevičius.
-// * @version 1.0 2015-09-05
-// */
-//class RealPixelGenerator(counter: Counter) extends PixelGenerator {
-//  override def generate(imageWidth: Int, imageHeight: Int, pxToCoord: NumericRange[Double]): Array[Int] = {
-//    val pixelCount = imageWidth * imageHeight
-//    val pixels = Array.ofDim[Int](pixelCount)
-//    for (xPos <- pixels.indices)
-//      pixels(xPos) = counter.getMax(pxToCoord.apply(xPos).floor.toInt)
-//    pixels
-//  }
-//}
+package mm.bitmap.gen
+
+import mm.bitmap.counters.Counter
+import mm.bitmap.numbers.ComplexAsIfIntegral
+import org.apache.commons.math3.complex.Complex
+
+import scala.collection.immutable.NumericRange
+import scala.math.Numeric.DoubleAsIfIntegral
+
+/**
+ * sifjdfg
+ * @author Martynas Maciulevičius.
+ * @version 1.0 2015-09-05
+ */
+class RealPixelGenerator(counter: Counter) extends PixelGenerator {
+  override def generate(imageWidth: Int, imageHeight: Int, pxToCoord: Iterator[Complex]): Array[Int] = {
+    val pixelCount = imageWidth * imageHeight
+    val pixels = Array.ofDim[Int](pixelCount)
+    for (xPos <- pixels.indices)
+      pixels(xPos) = counter.getMax(pxToCoord.next())
+    pixels
+  }
+}
