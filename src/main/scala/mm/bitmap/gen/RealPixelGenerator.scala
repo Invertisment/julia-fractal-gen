8ac6407d6e9be7b9b3fb602535bcf647664f786e
MartynasMaciulevicius <visiskaj.slaptaj@gmail.com>
2015-09-13 21:49:09 +0300

    RealPixelGen: multithreaded image generation

--- src/main/scala/mm/bitmap/gen/RealPixelGenerator.scala
+++ src/main/scala/mm/bitmap/gen/RealPixelGenerator.scala
@@ -1,35 +1,54 @@
 package mm.bitmap.gen
 
+import java.util.concurrent.CountDownLatch
+
 import mm.bitmap.counters.Counter
 import mm.bitmap.numbers.ComplexAsIfIntegral.complexIntegral
 import org.apache.commons.math3.complex.Complex
 
 import scala.collection.immutable.NumericRange
+import scala.concurrent.{ExecutionContext, Future}
+import scala.math.Numeric.DoubleAsIfIntegral
+import scala.util.Try
 
 /**
  * sifjdfg
  * @author Martynas Maciulevičius.
  * @version 1.0 2015-09-05
  */
-class RealPixelGenerator(counter: Counter) extends PixelGenerator {
+class RealPixelGenerator(counter: Counter)(implicit val executionContext: ExecutionContext) extends PixelGenerator {
   override def generate(count: Int, pxToCoord: Iterator[Complex]): Array[Int] =
-    Array.ofDim[Int](count)
-      .map((i: Int) => counter.getMax(pxToCoord.next()))
+    pxToCoord.map((complex: Complex) => counter.getMax(complex)).toArray
 
   override def generate(width: Int, height: Int, coordBounds: (Point, Point)): Array[Int] = {
     val minFirst = getMin(coordBounds)
     val maxFirst = getMax(coordBounds)
-    val stepFirst = new Complex(maxFirst.getReal - minFirst.getReal, minFirst.getImaginary).divide(width)
-    val firsts = NumericRange(minFirst, maxFirst, stepFirst).toIterator
+    val stepFirst = (maxFirst.getReal - minFirst.getReal) / width
+    val firsts = NumericRange(minFirst.getReal, maxFirst.getReal, stepFirst)(DoubleAsIfIntegral).toIterator
     val masterArray = Array.ofDim[Int](width * height)
+    val latch = new CancellableCountDownLatch(width)
     0 until width foreach ((horizontalPosition: Int) => {
-      val min = firsts.next()
+      if (latch.isAborted)
+        println("line " + horizontalPosition + " start")
+      val min = new Complex(firsts.next(), minFirst.getImaginary)
       val max = new Complex(min.getReal, maxFirst.getImaginary)
       val step = max.subtract(min).divide(height)
       val range = NumericRange(min, max, step).toIterator
-      val arr = generate(height, range)
-      arr.copyToArray(masterArray, height * horizontalPosition)
+      Future {
+        val arr = generate(height, range)
+        for (x <- arr.indices)
+          masterArray(x * width + horizontalPosition) = arr(x)
+        println("line " + horizontalPosition)
+      }(executionContext)
+        .onComplete((triedUnit: Try[Unit]) => {
+        if (triedUnit.isFailure) {
+          val thr = triedUnit.failed.get
+          latch.abort(thr)
+        }
+        latch.countDown()
+      })
     })
+    latch.await()
     masterArray
   }
 
--- src/test/scala/mm/bitmap/gen/RealPixelGeneratorSpec.scala
+++ src/test/scala/mm/bitmap/gen/RealPixelGeneratorSpec.scala
@@ -1,16 +1,27 @@
 package mm.bitmap.gen
 
+import java.util.concurrent.Executors
+
 import mm.bitmap.counters.Counter
 import org.apache.commons.math3.complex.Complex
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import mm.bitmap.numbers.ComplexAsIfIntegral.complexIntegral
 import scala.collection.immutable.NumericRange
+import scala.concurrent.ExecutionContext
 
 /**
  * @author Martynas Maciulevičius.
  * @version 1.0 2015-09-06
  */
-class RealPixelGeneratorSpec extends FlatSpec with Matchers {
+class RealPixelGeneratorSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
+
+  lazy val executor = Executors.newSingleThreadExecutor()
+  implicit lazy val context = ExecutionContext.fromExecutor(executor)
+
+  override protected def afterAll(): Unit = {
+    executor.shutdown()
+    super.afterAll()
+  }
 
   "RealImage" should "create desired size image" in {
     val desiredPixelColor = 12345
@@ -113,9 +124,56 @@ class RealPixelGeneratorSpec extends FlatSpec with Matchers {
       val out = generator.generate(length, yValue, (Point(0, 0), Point(length, length)))
       iterSkip shouldEqual length * yValue
       out.length shouldBe length * yValue
-      val numbers2 = (1 to yValue * length).iterator
+      //      val numbers2 = (1 to yValue * length).map((index: Int) => (index + yValue) % yValue).iterator
+      val numbers2 = (0 to yValue * length).map((i: Int) => i % length * yValue + i / length + 1).iterator
       out.foreach((i: Int) => i shouldEqual numbers2.next)
     })
+  }
+
+  it should "produce correct first values with negative input points" in {
+    val yValue = 1
+    val nullValue = -10
+    9 to 9 foreach ((length: Int) => {
+      val min = new Complex(nullValue, nullValue)
+      val max = new Complex(length, nullValue)
+      val step = max.subtract(min).divide(length)
+      val firstValues = NumericRange(min, max, step).iterator
+      val generator = new RealPixelGenerator(new Counter {
+        override def getMax(int: Complex): Int = fail()
+      }) {
+        override def generate(count: Int, pxToCoord: Iterator[Complex]): Array[Int] = {
+          count shouldBe yValue
+          val next = firstValues.next()
+          val next2 = pxToCoord.next()
+          next shouldBe next2
+          Array()
+        }
+      }
+      generator.generate(length, yValue, (Point(nullValue, nullValue), Point(length, length)))
+      firstValues.hasNext shouldBe false
+    })
+  }
+
+  "Min value of the sequence" should "be negative if range is negative" in {
+    val yValue = 1
+    val nullValue = -10
+    val length = 1
+    val generator = new RealPixelGenerator(new Counter {
+      override def getMax(int: Complex): Int = fail()
+    }) {
+      var isCalled = false
+
+      override def generate(count: Int, pxToCoord: Iterator[Complex]): Array[Int] = {
+        isCalled shouldBe false
+        isCalled = true
+        count shouldBe yValue
+        val next2 = pxToCoord.next()
+        next2 shouldBe new Complex(nullValue, nullValue)
+        Array()
+      }
+    }
+    generator.generate(length, yValue, (Point(nullValue, nullValue), Point(length, length)))
+    generator.isCalled shouldBe true
   }
 
 }
